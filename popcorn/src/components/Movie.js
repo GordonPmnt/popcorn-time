959ea5afc8b2c81393a3cbd2fe746cf15824392e
Boucqueau <gyboucqueau@hotmail.com>
2020-05-08 09:52:19 +0200

    creation of the Movie component

---
+++ popcorn/src/components/Movie.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Movie extends React.Component {
+    state = {
+    }
+
+    render () {
+        return (
+            <div>
+                MOVIES
+            </div>
+        )
+    }
+}
+
+export default Movie;
