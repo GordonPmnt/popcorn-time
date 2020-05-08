825dfcd3ec9e84a8224865a6c3ca751150361f8f
Gordon Preumont <preumontgr@gmail.com>
2020-05-08 10:59:10 +0200

    display movies

--- popcorn-time/src/components/Movie.js
+++ popcorn-time/src/components/Movie.js
@@ -2,7 +2,24 @@ import React from 'react';
 
 
 const Movie = ({ id, title }) => {
-    return <p>{title}</p>
+    const styles = {
+        p: {
+            margin: '1px'
+        },
+        button: {
+            marginRight: '10px'
+        },
+    }
+
+    const movie = title
+
+    return (
+        <div>
+            <p style={styles.p}>
+                {movie}
+            </p>
+        </div>
+    )
 }
 
 export default Movie;
