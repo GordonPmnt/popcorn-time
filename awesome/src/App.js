734daf1799c7c1d15d23fdc17c48084d18b35865
Guillaume-Kreit <Guillaume.kreit@outllook.com>
2020-05-08 12:24:25 +0200

    axios

--- awesome/src/App.js
+++ awesome/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import Movie from './components/Movie';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Popcorn Time</h1>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    moviesList: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://raw.githubusercontent.com/wildcodeschoolparis/datas/master/movies.json`)
+      .then(res => {
+        console.log(res)
+        // const persons = res.data;
+        // this.setState({ persons });
+      })
+  }
+
+  render() {
+    return (
+       
+        <ul>
+          { this.state.moviesList.map(movie => <Movie title={movie.title}/>)}
+        </ul>
+    )
+  }
 }
 
-export default App;
---
+++ awesome/src/components/Movie.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Movie = () => {
+    
+   
+    
+    return (
+     <h1>Popcorn Time</h1>   
+    )
+}
+
+
+export default Movie;
